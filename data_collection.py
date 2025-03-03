99c8cb9044ebd1dd5beb2a6298b5079d605a8702
Clarexz <calebmartinez0929@gmail.com>
2025-03-03 13:17:06 -0600

    Agregué estilos a las coordenadas

--- data_collection.py
+++ data_collection.py
@@ -38,13 +38,21 @@ def capture_data():
 
             # Dibujar landmarks
             if results.face_landmarks:
-                mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION)
+                mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
+                                        mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
+                                        mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1))
             if results.pose_landmarks:
-                mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+                mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                                        mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4),
+                                        mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2))
             if results.left_hand_landmarks:
-                mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+                mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                                        mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4),
+                                        mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2))
             if results.right_hand_landmarks:
-                mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+                mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                                        mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
+                                        mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2))
 
             # Mostrar el frame en una ventana
             cv2.imshow("Cámara", image)
